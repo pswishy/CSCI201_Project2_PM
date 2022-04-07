e5ffb2ca12080c92180818c67ff97933679d98b7
Pierce Medy <piercemedy24@gmail.com>
2022-04-07 15:19:11 -0400

    get user input. max char input 1000

--- project2.s
+++ project2.s
@@ -2,3 +2,11 @@
 userInput:  .space 1001 # allow user to input string of 1000 characters
 
 .text
+
+main: 
+
+      #-----------------------
+      li $v0, 8  # gets user input from keyboard
+      la $a0, userInput # stores user in put in $a0
+      li $a1, 1001 # max lengeth of user input string
+      syscall
