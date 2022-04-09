38ac65a932f5b1f3d0d0f061afb766dafc128521
Pierce Medy <piercemedy24@gmail.com>
2022-04-09 16:35:36 -0400

    more logic if space or tab character is found

--- project2.s
+++ project2.s
@@ -20,10 +20,14 @@ while:
       lb $s1, 0($t9)
       beq $t1, 1000, exit
       beq $s1, 9, tabOrSpaceCharFound # if the char is a tab we have to give special consideration
-
+      beq $s1, 32, tabOrSpaceCharFound # 32 = space char, 9 = tab char
 tabOrSpaceCharFound:
       # if it is a tab or space char and len $t0 is 0 then we want to ignore because it is a leading whitespace 
+      beq $t0, 0, skip # if t0 equals 0 leading whitespace dont update length of userinput string
 
+      addi $t9, $t9, 1 # increment loop address for loop
+      addi $t1, $t1, 1 # increment loop break condition
+      add $t0, $t0, 1 # increment length of userinput string by 1 if space or tab char is not a leading whitespace char 
 
 
 exit:
