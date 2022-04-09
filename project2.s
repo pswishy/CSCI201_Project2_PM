e3b4513014fc87dfb995dd918429cad78cc10899
Pierce Medy <piercemedy24@gmail.com>
2022-04-09 17:41:28 -0400

    error qtspim no longer responding when i run

--- project2.s
+++ project2.s
@@ -1,14 +1,13 @@
 .data
 userInput:  .space 1001 # allow user to input string of 1000 characters
-error .asciiz "Not recognized"
+
 .text
 
 main: 
-
       #-----------------------
       li $v0, 8  # gets user input from keyboard
       la $a0, userInput # stores user in put in $a0
-      li $a1, 1001 # max lengeth of user input string
+      li $a1, 10 # max lengeth of user input string
       syscall
 
       # first step count how many chars are in userinput while ignoring blank and space tabs
@@ -16,55 +15,51 @@ main:
       li $t1, 0 # iterator variable
       li $t2, 0 # exponent tracker
       li $t3, 33 # base for my program
-
+      li $t4, 0 # sum variable
       la $t9, userInput
 
 
 # where we will count length of userinput
-while:  
-      lb $s1, 0($t9)
-      beq $t1, 1000, exit
-      beq $s1, 9, tabOrSpaceCharFound # if the char is a tab we have to give special consideration
-      beq $s1, 32, tabOrSpaceCharFound # 32 = space char, 9 = tab char
+@ while:  
+@       lb $s1, 0($t9)
+@       beq $t1, 1000, exit
+@       beq $s1, 9, tabOrSpaceCharFound # if the char is a tab we have to give special consideration
+@       beq $s1, 32, tabOrSpaceCharFound # 32 = space char, 9 = tab char
 
-      bgt $t0, 4, errorMessage # if length t0 greater than 4 print error message exit
+      # bgt $t0, 4, errorMessage # if length t0 greater than 4 print error message exit
       
       
-      blt $s1, 48, errorMessage # 48 = '0' in ascii. if char < 48 invalid char. print message and exit
+      # blt $s1, 48, errorMessage # 48 = '0' in ascii. if char < 48 invalid char. print message and exit
 
       # start logic for valid char. (may have to update bgt t0 4 line)
       # pass char as argument
-      addi $a1, $s1, 0 # a1 hold 1 argument pass to add num function
-      jal sumNum
+      # addi $a2, $s1, 0 # a1 hold 1 argument pass to add num function
+      # jal sumNum
 
 
-tabOrSpaceCharFound:
+# tabOrSpaceCharFound:
       # if it is a tab or space char and len $t0 is 0 then we want to ignore because it is a leading whitespace 
-      beq $t0, 0, skip # if t0 equals 0 leading whitespace dont update length of userinput string
+     # beq $t0, 0, skip # if t0 equals 0 leading whitespace dont update length of userinput string
       
       # if $t0 is not equal to 0 then tab or space char is a invalid char because it not in our range. 
-      j errorMessage
+      # j errorMessage
 
-skip:
-      addi $t9, $t9, 1 # increment loop address for loop
-      addi $t1, $t1, 1 # increment loop break condition
-      j while
+@ skip:
+@       addi $t9, $t9, 1 # increment loop address for loop
+@       addi $t1, $t1, 1 # increment loop break condition
+@       j while
 
-errorMessage:
-      # print error message
-      li $v0, 4
-      la $a0, error
-      syscall
+@ errorMessage:
+@       # print error message
+@       li $v0, 4
+@       la $a0, error
+@       syscall
 
-      j exit
+@       j exit
 
-sumnum:
-      sub $s1, $s1, 48 # s1 = s1 - 48/ subtract 48 to get correct final sum val
-      add $t0, $t0, $s1 # add val to $t0
-      sub $s1, $s1, $s1 # set $s1 back to 0
-      addi $t9, $t9, 1 # increment address for loop
-      addi $t1, $t1, 1
-      j while
+@ # sumnum:
+     
+
 exit:
 
       li $v0, 10
